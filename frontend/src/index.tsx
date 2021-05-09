8aee506e30ff8401c305d7d5603ff73f44396a29
Henrique Miranda <miranda.henrique@outlook.com.br>
2021-05-08 21:11:46 -0300

    Bootstrap

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'assets/css/styles.css'
 import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <h1 className = "text-primary">Olá, mundo!</h1>
   </React.StrictMode>,
   document.getElementById('root')
 );
